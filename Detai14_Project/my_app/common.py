1450cf399ac4721da1a6e252652be3a353ec4bd0
DaoTien123 <daothicamtien0120@gmail.com>
2022-06-09 02:31:12 +0700

    add

---
+++ Detai14_Project/my_app/common.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request, render_template, make_response, redirect, url_for, session, flash
+from flask_sqlalchemy import SQLAlchemy
+from my_app import app, db
+import requests
+from urllib.parse import quote
+from my_app.forms import CreateTableForm
+import sys
+import pymysql
+import json
+
+def InfoConnection():
+    rds_host = "nhom14.cpaj91orup1s.us-east-1.rds.amazonaws.com"
+    name = "admin"
+    password = "123456789"
+    db_name = ""
+
+    try:
+        conn = pymysql.connect(host=rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
+    except pymysql.MySQLError as e:
+        print(e)
+        sys.exit()
+
+    return conn
---
+++ Detai14_Project/my_app/models.py
@@ -0,0 +1,48 @@
+from sqlalchemy.util import text_type
+
+from my_app import db
+from flask_login import UserMixin
+from datetime import datetime
+from flask_login.mixins import *
+
+class Users(db.Model, UserMixin):
+    Id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    FullName = db.Column(db.String(length=200), nullable=True)
+    UserName = db.Column(db.String(length=100), nullable=True, unique=True)
+    Password = db.Column(db.String(length=200), nullable=False)
+    Email = db.Column(db.String(length=200), nullable=False, unique=True)
+    Phone = db.Column(db.String(length=20), nullable=True)
+    IsAdmin = db.Column(db.Integer(), nullable=False)
+    IsDelete = db.Column(db.Integer(), nullable=False, default = 0)
+    Active = db.Column(db.Integer(), nullable=False)
+    Avatar = db.Column(db.String(length=200), nullable=True)
+
+    def get_id(self):
+        try:
+            return text_type(self.Id)
+        except AttributeError:
+            raise NotImplementedError('No `id` attribute - override `get_id`')
+
+class UserCreateDatabase(db.Model):
+    __tablename__ = "UserCreateDatabase"
+    Id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    Name = db.Column(db.String(length=200), nullable=False)
+    IdUserCreate = db.Column(db.Integer(), nullable=False)
+    CreateDate = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    Active = db.Column(db.Integer(), nullable=False)
+
+class UserCreateTable(db.Model):
+    __tablename__ = "InfoAddRowDatabase"
+    Id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    NameDatabase = db.Column(db.String(length=200), nullable=False)
+    NameTable = db.Column(db.String(length=200), nullable=False)
+
+class Datatype(db.Model):
+    __tablename__ = "Datatype"
+    Id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    Name = db.Column(db.String(length=50), nullable=False)
+    CheckInputNumber = db.Column(db.Integer(), nullable=False)
+    Active = db.Column(db.Integer(), nullable=False)
+
+if __name__ == '__main__':
+    db.create_all()
